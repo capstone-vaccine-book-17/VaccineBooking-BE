cc3a000dfe436dad4cb89d786880e5c2537cc129
dika2015091057 <dika.rama@undiksha.ac.id>
2022-12-04 19:35:37 +0800

    add dto fammily

--- dto/citizenDto/family.go
+++ dto/citizenDto/family.go
@@ -1,12 +1,20 @@
 package citizenDto
 
-type fammylDTO struct{
-	FamilyID   uint      `json:"family_id"`
-	Relation string      `json:"relation"`
-	Name       string    `json:"name"`
-	Nik        string    `json:"nik"`
-	Age        uint      `json:"age"`
-	Gender     string    `json:"gender"`
+type FammylDTO struct {
+	FamilyId uint   `json:"family_id"`
+	Relation string `json:"relation"`
+	Name     string `json:"name"`
+	Nik      string `json:"nik"`
+	Age      uint   `json:"age"`
+	Gender   string `json:"gender"`
 }
 
-type 
+type FamilyReq struct {
+	CitizenId uint   `json:"citizen_id"`
+	FamilyId  uint   `json:"family_id"`
+	Relation  string `json:"relation"`
+	Name      string `json:"name"`
+	Nik       string `json:"nik"`
+	Age       uint   `json:"age"`
+	Gender    string `json:"gender"`
+}
