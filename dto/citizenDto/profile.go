01dc5db1a70079ef28e002d36dab2678295f65cd
dika2015091057 <dika.rama@undiksha.ac.id>
2022-12-04 18:23:51 +0800

    fix

--- dto/citizenDto/profile.go
+++ dto/citizenDto/profile.go
@@ -47,4 +47,5 @@ type UpdatePassword struct {
 	OldPassword string `json:"old_password" validate:"required"`
 	NewPassword string `json:"new_password" validate:"required"`
 	ConfirmNewPassword string `json:"confirm_new_password" validate:"required"`
+	
 }
