f8c4c8c62718d8d8ad3c887e90171334f7bd4496
dika2015091057 <dika.rama@undiksha.ac.id>
2022-12-05 13:31:56 +0800

    update model

--- model/family_member.go
+++ model/family_member.go
@@ -3,15 +3,14 @@ package model
 import "time"
 
 type FamilyMember struct {
-	FamilyID   uint      `gorm:"primaryKey;autoIncrement" json:"family_id"`
-	Citizen    Citizen   `gorm:"foreignKey:CitizenId"`
-	CitizenId  uint      `json:"citizen_id"`
-	FamilyAs   FamilyAs  `gorm:"foreignKey:FamilyAsId"`
-	FamilyAsId uint      `json:"family_as_id"`
-	Name       string    `gorm:"size:50;not null" json:"name"`
-	Nik        string    `gorm:"size:16;not null" json:"nik"`
-	Age        uint      `gorm:"age"`
-	Gender     string    `gorm:"size:15;not null" json:"gender"`
-	CreatedAT  time.Time `json:"created_at"`
-	UpdatedAT  time.Time `json:"updated_at"`
+	FamilyID  uint      `gorm:"primaryKey;autoIncrement" json:"family_id"`
+	Citizen   Citizen   `gorm:"foreignKey:CitizenId"`
+	CitizenId uint      `json:"citizen_id"`
+	FamilyAs  string    `gorm:"size:15;not null" json:"family_as"`
+	Name      string    `gorm:"size:50;not null" json:"name"`
+	Nik       string    `gorm:"size:16;not null" json:"nik"`
+	Age       uint      `gorm:"age"`
+	Gender    string    `gorm:"size:15;not null" json:"gender"`
+	CreatedAT time.Time `json:"created_at"`
+	UpdatedAT time.Time `json:"updated_at"`
 }
