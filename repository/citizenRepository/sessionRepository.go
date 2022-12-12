03f0c90eb8b55b8cf570a925585031902fbccacd
fachrudin27 <mfachrudin27@gmail.com>
2022-12-12 22:05:14 +0700

    add conv_date from DTO

--- repository/citizenRepository/sessionRepository.go
+++ repository/citizenRepository/sessionRepository.go
@@ -9,7 +9,7 @@ import (
 func (u *citizenRepository) GetSessionByMedicalId(medicalID uint) ([]citizenDto.SessionDto, error) {
 
 	var session []citizenDto.SessionDto
-	if err := u.db.Model(&model.Session{}).Select("sessions.*, vaccine_varieties.name as vaccine").
+	if err := u.db.Model(&model.Session{}).Select("sessions.session_id, sessions.name, sessions.kuota, sessions.dosis, sessions.date, DATE_FORMAT(sessions.date, '%d %M %Y') as conv_date, sessions.start_time, sessions.end_time, vaccine_varieties.name as vaccine").
 		Joins("JOIN vaccine_varieties on vaccine_varieties.vaccine_id = sessions.vaccine_id").
 		Where("sessions.medical_facilitys_id = ? AND sessions.status = 'process'", medicalID).Find(&session).Error; err != nil {
 		return nil, err
