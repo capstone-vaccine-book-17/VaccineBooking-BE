3f28bc0a8decc6f503459ffe9c4ab032e824763d
dika2015091057 <dika.rama@undiksha.ac.id>
2022-12-17 15:58:53 +0800

    uncomment router

--- router/router.go
+++ router/router.go
@@ -2,9 +2,12 @@ package router
 
 import (
 	"capstone_vaccine/controller/adminController"
+	"capstone_vaccine/controller/citizenController"
 	m "capstone_vaccine/middleware"
 	"capstone_vaccine/repository/adminRepository"
+	"capstone_vaccine/repository/citizenRepository"
 	"capstone_vaccine/service/adminService"
+	"capstone_vaccine/service/citizenService"
 	"capstone_vaccine/utils"
 	"os"
 
@@ -22,23 +25,23 @@ func New(e *echo.Echo, db *gorm.DB) {
 
 	// TODO REPOSITORY
 	adminRepository := adminRepository.NewAdminRepository(db)
-	// citizenRepository := citizenRepository.NewCitizenRepository(db)
+	citizenRepository := citizenRepository.NewCitizenRepository(db)
 
 	// TODO SERVICE
 	adminService := adminService.NewAdminService(adminRepository)
-	// citizenService := citizenService.NewCitizenService(citizenRepository)
+	citizenService := citizenService.NewCitizenService(citizenRepository)
 
 	// TODO CONTROLLER
 	adminController := adminController.NewAdminController(adminService)
-	// citizenController := citizenController.NewCitizenController(citizenService)
+	citizenController := citizenController.NewCitizenController(citizenService)
 
 	// TODO ADMIN ROUTE
 
 	v1 := e.Group("/v1")
 	v1.Use(middleware.JWT([]byte(os.Getenv("JWT_KEY"))))
 
-	// v2 := e.Group("/v2")
-	// v2.Use(middleware.JWT([]byte(os.Getenv("JWT_KEY"))))
+	v2 := e.Group("/v2")
+	v2.Use(middleware.JWT([]byte(os.Getenv("JWT_KEY"))))
 
 	v1.GET("/", adminController.GetDashboard)
 
@@ -48,25 +51,25 @@ func New(e *echo.Echo, db *gorm.DB) {
 	// TODO REGISTER
 	v1.POST("/register", adminController.RegisterAdmin, m.Authorization)
 
-	// // TODO MEDICAL FACILITYS
-	// v1_medical := v1.Group("/medical")
-	// {
-	// 	v1_medical.POST("/", adminController.CreateMedical, m.Authorization)
-	// }
+	// TODO MEDICAL FACILITYS
+	v1_medical := v1.Group("/medical")
+	{
+		v1_medical.POST("/", adminController.CreateMedical, m.Authorization)
+	}
 
-	// // TODO ROLES
+	// TODO ROLES
 
-	// v1_roles := v1.Group("/role")
-	// v1_roles.POST("/", adminController.CreateRoles, m.Authorization)
+	v1_roles := v1.Group("/role")
+	v1_roles.POST("/", adminController.CreateRoles, m.Authorization)
 
-	// v1_session := v1.Group("/session")
-	// {
-	// 	v1_session.POST("/", adminController.CreateSession)
-	// 	v1_session.GET("/", adminController.GetAllSession)
-	// 	v1_session.GET("/:id", adminController.GetSessionById)
-	// 	v1_session.PUT("/:id", adminController.UpdateSession)
-	// 	v1_session.DELETE("/:id", adminController.DeleteSession)
-	// }
+	v1_session := v1.Group("/session")
+	{
+		v1_session.POST("/", adminController.CreateSession)
+		v1_session.GET("/", adminController.GetAllSession)
+		v1_session.GET("/:id", adminController.GetSessionById)
+		v1_session.PUT("/:id", adminController.UpdateSession)
+		v1_session.DELETE("/:id", adminController.DeleteSession)
+	}
 
 	v1_manageVaccine := v1.Group("/vaccine")
 	{
@@ -84,76 +87,76 @@ func New(e *echo.Echo, db *gorm.DB) {
 		v1_profile.PUT("/image", adminController.UpdateImage)
 	}
 
-	// v1_booking := v1.Group("/booking")
-	// {
-	// 	v1_booking.GET("/", adminController.GetAllBooking)
-	// 	v1_booking.POST("/", adminController.CreateBooking)
-	// 	v1_booking.PUT("/:id", adminController.UpdateBooking)
-	// 	v1_booking.GET("/:id", adminController.GetBookingById)
-	// 	v1_booking.DELETE("/:id", adminController.DeleteBooking)
-	// }
+	v1_booking := v1.Group("/booking")
+	{
+		v1_booking.GET("/", adminController.GetAllBooking)
+		v1_booking.POST("/", adminController.CreateBooking)
+		v1_booking.PUT("/:id", adminController.UpdateBooking)
+		v1_booking.GET("/:id", adminController.GetBookingById)
+		v1_booking.DELETE("/:id", adminController.DeleteBooking)
+	}
 
 	// // TODO CITIZEN ROUTE
 
-	// // Citizen Auth
-	// e.POST("/signup", citizenController.RegisterCitizen)
-	// e.POST("/signin", citizenController.LoginCitizen)
+	// Citizen Auth
+	e.POST("/signup", citizenController.RegisterCitizen)
+	e.POST("/signin", citizenController.LoginCitizen)
 
-	// //profile
-	// v2_profile := v2.Group("/profile")
-	// {
+	//profile
+	v2_profile := v2.Group("/profile")
+	{
 
-	// 	v2_profile.GET("/", citizenController.GetProfile)
-	// 	v2_profile.PUT("/image", citizenController.UploadImage)
-	// 	v2_profile.GET("/personal", citizenController.GetPersonalData)
-	// 	v2_profile.GET("/address", citizenController.GetAddress)
-	// 	v2_profile.PUT("/updateAddress", citizenController.UpdateAddress)
-	// 	v2_profile.GET("/email", citizenController.GetEmail)
-	// 	v2_profile.PUT("/emailUpdate", citizenController.UpdateEmail)
-	// 	v2_profile.PUT("/passwordUpdate", citizenController.UpdatePassword)
+		v2_profile.GET("/", citizenController.GetProfile)
+		v2_profile.PUT("/image", citizenController.UploadImage)
+		v2_profile.GET("/personal", citizenController.GetPersonalData)
+		v2_profile.GET("/address", citizenController.GetAddress)
+		v2_profile.PUT("/updateAddress", citizenController.UpdateAddress)
+		v2_profile.GET("/email", citizenController.GetEmail)
+		v2_profile.PUT("/emailUpdate", citizenController.UpdateEmail)
+		v2_profile.PUT("/passwordUpdate", citizenController.UpdatePassword)
 
-	// }
-	// v2_family := v2.Group("/family")
-	// {
-	// 	v2_family.GET("/", citizenController.GetFamilys)
-	// 	v2_family.POST("/", citizenController.CreateFamilyMember)
-	// 	v2_family.DELETE("/:id", citizenController.DeleteMember)
-	// 	v2_family.GET("/:id", citizenController.GetDetailMember)
-	// }
+	}
+	v2_family := v2.Group("/family")
+	{
+		v2_family.GET("/", citizenController.GetFamilys)
+		v2_family.POST("/", citizenController.CreateFamilyMember)
+		v2_family.DELETE("/:id", citizenController.DeleteMember)
+		v2_family.GET("/:id", citizenController.GetDetailMember)
+	}
 
-	// // Citizen Search Medical For Booking
-	// v2_medical := v2.Group("/medical")
-	// {
-	// 	// By Search and Sort
-	// 	v2_medical.GET("/", citizenController.GetMedicalByCity)
+	// Citizen Search Medical For Booking
+	v2_medical := v2.Group("/medical")
+	{
+		// By Search and Sort
+		v2_medical.GET("/", citizenController.GetMedicalByCity)
 
-	// 	// By ID
-	// 	v2_medical.GET("/:medicalID", citizenController.GetMedicalById)
-	// }
+		// By ID
+		v2_medical.GET("/:medicalID", citizenController.GetMedicalById)
+	}
 
-	// // Citizen Session
-	// v2_session := v2.Group("/session")
-	// {
-	// 	v2_session.GET("/:medicalID", citizenController.GetSessionByMedicalId)
-	// }
+	// Citizen Session
+	v2_session := v2.Group("/session")
+	{
+		v2_session.GET("/:medicalID", citizenController.GetSessionByMedicalId)
+	}
 
-	// // Citizen Booking
-	// v2_booking := v2.Group("/booking")
-	// {
-	// 	// Create Booking
-	// 	v2_booking.POST("/", citizenController.CreateBooking)
+	// Citizen Booking
+	v2_booking := v2.Group("/booking")
+	{
+		// Create Booking
+		v2_booking.POST("/", citizenController.CreateBooking)
 
-	// Get Last Booking For Ticket
-	// 	v2_booking.GET("/", citizenController.GetLastBooking)
-	// }
+		// Get Last Booking For Ticket
+		v2_booking.GET("/", citizenController.GetLastBooking)
+	}
 
 	//TICKET
-	// v2_ticket := v2.Group("/ticket")
-	// {
-	// 	v2_ticket.GET("/", citizenController.GetAllTicket)
-	// 	v2_ticket.GET("/:id", citizenController.GetTicket)
-	// 	v2_ticket.GET("/proses", citizenController.GetTicketOnProses)
-	// 	v2_ticket.GET("/finish", citizenController.GetTicketOnFinish)
+	v2_ticket := v2.Group("/ticket")
+	{
+		v2_ticket.GET("/", citizenController.GetAllTicket)
+		v2_ticket.GET("/:id", citizenController.GetTicket)
+		v2_ticket.GET("/proses", citizenController.GetTicketOnProses)
+		v2_ticket.GET("/finish", citizenController.GetTicketOnFinish)
 
-	// }
+	}
 }
